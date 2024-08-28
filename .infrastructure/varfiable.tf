e1d93452ce49df3024970052be2de2628531b155
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-08-28 12:34:51 +0300

    Update varfiable.tf

--- .infrastructure/varfiable.tf
+++ .infrastructure/varfiable.tf
@@ -4,3 +4,20 @@ variable "region" {
 variable "ecr_name" {
   default = "hello"
 }
+variable "ecs_cluster_name" {
+  default = "hello"
+} 
+  
+variable "ecs_task_family" {
+  default = "hello"
+}
+variable "ecs_container_name" {
+  default = "hello"
+}
+
+variable "pr_list" {
+  type = list(string)
+  default = [ "q5"]
+}
+
+
