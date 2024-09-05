dea529ffeae9cd6bd068ed1083aab8fc54fc7d6d
Vadym Trufyn <148262369+VadimTrufyn@users.noreply.github.com>
2024-09-05 11:29:52 +0300

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -6,7 +6,7 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"hello Misha")
+        self.wfile.write(b"2")
 
 def run(server_class=HTTPServer, handler_class=HelloHandler, port=80):
     server_address = ('', port)
